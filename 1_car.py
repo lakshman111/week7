a2307b3583120ec1fe926bb4667277e06ad02124
Lakshman Mody <lakshman111@gmail.com>
2016-02-24 18:30:05 -0600

    commented up notes

--- 1_car.py
+++ 1_car.py
@@ -1,14 +1,24 @@
+import random
+
 class Car:
 
-  def __init__(self):
+  def __init__(self, value):
     self.speed = 0
-    self.odometer = 0
+    self.odometer = value
+    vin = []
+    for i in range(10):
+      vin.append(str(random.randint(0,9)))
+    self.vin = ''.join(vin)
+
 
   def set_speed(self, new_speed):
     self.speed = new_speed
 
   def drive_for(self, minutes):
     self.odometer += (self.speed * minutes/60)
+
+  # def uniqe_vin(self, vin_number):
+  #   self.vin = vin_number
 
   def needs_oil_change(self):
     return (self.odometer >= 3000)
@@ -17,19 +27,21 @@ class Car:
 # Let's drive!
 # Run this code to see it in action.
 
-car1 = Car()
+car1 = Car(30)
 print("Odometer for car 1 starts at:", car1.odometer, "miles")
 car1.set_speed(40)
 car1.drive_for(30)
 print("Odometer for car 1 is now:", car1.odometer, "miles")
+print("VIN number for car 1 is:", car1.vin)
 if car1.needs_oil_change():
   print("Car 1 needs an oil change!")
 
-car2 = Car()
+car2 = Car(30)
 print("Odometer for car 2 starts at:", car2.odometer, "miles")
 car2.set_speed(80)
 car2.drive_for(30)
 print("Odometer for car 2 is now:", car2.odometer, "miles")
+print("VIN number for car 2 is:", car2.vin)
 if car2.needs_oil_change():
   print("Car 2 needs an oil change!")
 
@@ -37,4 +49,5 @@ if car2.needs_oil_change():
 # classes and distinct object instances.
 
 # CHALLENGE 1: Can we provide an option to begin each car at a certain odometer value?
+# done
 # CHALLENGE 2: Can you assign a unique VIN to each car?
--- 2_linked_list.py
+++ 2_linked_list.py
@@ -17,17 +17,80 @@
 #
 # Then we can try to teach the computer to do the same.
 
+# want to define the linked-list structure he drew on the board
+class Item:
+  def __init__(self):
+    # whats in the item?
+    self.data = None
+    # whats next in the list?
+    self.next = None
+
+
+class GroceryList:
+  def __init__(self):
+    self.head = None
+
+  def remove_all(self):
+    # if you delete the head, the whole list is lost
+    self.head = None
+    
+  def append(self, data):
+    # IMPORTANT: this is why all elements are items
+    new_item = Item()
+    new_item.data = data
+
+    # make the new item the head
+    if self.head is None:
+      self.head = new_item
+    # start at the head, work your way through the list
+    # set the new item = the .next item of the last in the list
+    else:
+      current_item = self.head
+      while (current_item.next != None):
+        current_item = current_item.next
+      current_item.next = new_item
+
+  def at(self, position):
+    # start at the head (Apples)
+    current_item = self.head
+    # count over to the position
+    while position > 0 and current_item != None:
+      # current_item = apples.next
+      current_item = current_item.next
+      position -= 1
+
+    if current_item != None:
+      # apples.next.data
+      return current_item.data
+    else:
+      return None
+
+  def length(self):
+    # start at head and cycle through to find length
+    n = 0
+    current_item = self.head
+    while (current_item != None):
+      n += 1
+      current_item = current_item.next
+
+    return n
+
+
 groceries = GroceryList()
 assert 0 == groceries.length()
 
 groceries.append("Apples")
 assert 1 == groceries.length()
-assert "Apples" == groceries[0]
+assert "Apples" == groceries.at(0)
 
 groceries.append("Bananas")
 assert 2 == groceries.length()
-assert "Bananas" == groceries[1]
+assert "Bananas" == groceries.at(1)
 
 groceries.append("Cookies")
 assert 3 == groceries.length()
-assert "Cookies" == groceries[2]
+assert "Cookies" == groceries.at(2)
+
+groceries.remove_all()
+assert 0 == groceries.length()
+assert None == groceries.at(2)
